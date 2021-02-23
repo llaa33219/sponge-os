tomga/genode

riscv: Use Qemu and update to ISA v1.10
- remove Spike/BBL support in favour of Qemu (>=4.2.1)
- add 'riscv_qemu' board, remove 'spike' board'
- update to privileged ISA v1.10 (from v1.9.1)
- use direct system calls for privileged core threads (they call into
  the kernel and don't use mode changing system calls, i.e. 'ecall',
  semantics)
- use 'OpenSBI' semtantics for SBI calls (to machine mode) instead of
  BBL

issue #4012

repos/base-hw/lib/mk/spec/riscv/bootstrap-hw-riscv_qemu.mk
@@ -1,0 +1,10 @@
+INC_DIR += $(REP_DIR)/src/bootstrap/spec/riscv_virt
+
+SRC_CC  += bootstrap/spec/riscv/platform.cc
+SRC_S   += bootstrap/spec/riscv/crt0.s
+SRC_CC  += lib/base/riscv/kernel/interface.cc
+SRC_CC  += spec/64bit/memory_map.cc
+
+vpath spec/64bit/memory_map.cc $(REP_DIR)/src/lib/hw
+
+include $(REP_DIR)/lib/mk/bootstrap-hw.inc

repos/base-hw/lib/mk/spec/riscv/core-hw-riscv_qemu.mk
@@ -1,11 +1,4 @@
-#
-# evaluate bbl_dir immediately, otherwise it won't recognize
-# missing ports when checking library dependencies
-#
-
-REP_INC_DIR += src/core/spec/riscv
-
-INC_DIR += $(call select_from_ports,bbl)/src/lib/bbl
+REP_INC_DIR += src/core/spec/riscv src/core/spec/riscv_virt
 
 CC_OPT += -fno-delete-null-pointer-checks
 
@@ -14,10 +7,11 @@ SRC_CC += platform_services.cc
 SRC_CC += kernel/vm_thread_off.cc
 SRC_CC += kernel/cpu_up.cc
 SRC_CC += kernel/lock.cc
-SRC_CC += spec/riscv/cpu.cc
 SRC_CC += spec/riscv/kernel/thread.cc
 SRC_CC += spec/riscv/kernel/cpu.cc
+SRC_CC += spec/riscv/kernel/interface.cc
 SRC_CC += spec/riscv/kernel/pd.cc
+SRC_CC += spec/riscv/cpu.cc
 SRC_CC += spec/riscv/platform_support.cc
 SRC_CC += spec/riscv/timer.cc
 SRC_CC += spec/64bit/memory_map.cc

llaa33219/sponge-os

repos/base-hw/lib/mk/spec/riscv/bootstrap-hw-riscv_qemu.mk
@@ -1,0 +1,10 @@
+INC_DIR += $(REP_DIR)/src/bootstrap/board/riscv_qemu
+
+SRC_CC  += bootstrap/spec/riscv/platform.cc
+SRC_S   += bootstrap/spec/riscv/crt0.s
+SRC_CC  += lib/base/riscv/kernel/interface.cc
+SRC_CC  += spec/64bit/memory_map.cc
+
+vpath spec/64bit/memory_map.cc $(REP_DIR)/src/lib/hw
+
+include $(REP_DIR)/lib/mk/bootstrap-hw.inc

repos/base-hw/lib/mk/spec/riscv/core-hw-riscv_qemu.mk
@@ -1,11 +1,4 @@
-#
-# evaluate bbl_dir immediately, otherwise it won't recognize
-# missing ports when checking library dependencies
-#
-
-REP_INC_DIR += src/core/spec/riscv
-
-INC_DIR += $(call select_from_ports,bbl)/src/lib/bbl
+REP_INC_DIR += src/core/spec/riscv src/core/board/riscv_qemu
 
 CC_OPT += -fno-delete-null-pointer-checks
 
@@ -14,10 +7,11 @@ SRC_CC += platform_services.cc
 SRC_CC += kernel/vm_thread_off.cc
 SRC_CC += kernel/cpu_up.cc
 SRC_CC += kernel/lock.cc
-SRC_CC += spec/riscv/cpu.cc
 SRC_CC += spec/riscv/kernel/thread.cc
 SRC_CC += spec/riscv/kernel/cpu.cc
+SRC_CC += spec/riscv/kernel/interface.cc
 SRC_CC += spec/riscv/kernel/pd.cc
+SRC_CC += spec/riscv/cpu.cc
 SRC_CC += spec/riscv/platform_support.cc
 SRC_CC += spec/riscv/timer.cc
 SRC_CC += spec/64bit/memory_map.cc