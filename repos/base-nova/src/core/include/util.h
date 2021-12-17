genodelabs/genode

base: avoid implicit conversions
This patch is a prerequisite for compiling the code with
the warnings -Wconversion enabled.

Issue #23

repos/base-nova/src/core/include/util.h
@@ -26,9 +26,12 @@ namespace Genode {
 
 	constexpr size_t get_super_page_size_log2() { return 22; }
 	constexpr size_t get_super_page_size()      { return 1 << get_super_page_size_log2(); }
-	inline addr_t trunc_page(addr_t addr)    { return addr & _align_mask(get_page_size_log2()); }
-	inline addr_t round_page(addr_t addr)    { return trunc_page(addr + get_page_size() - 1); }
 
+	template <typename T>
+	inline addr_t trunc_page(T addr) { return addr & _align_mask(get_page_size_log2()); }
+
+	template <typename T>
+	inline addr_t round_page(T addr) { return trunc_page(addr + get_page_size() - 1); }
 
 	inline addr_t map_src_addr(addr_t /* core_local */, addr_t phys) { return phys; }
 

llaa33219/sponge-os

repos/base-nova/src/core/include/util.h
@@ -26,9 +26,12 @@ namespace Genode {
 
 	constexpr size_t get_super_page_size_log2() { return 22; }
 	constexpr size_t get_super_page_size()      { return 1 << get_super_page_size_log2(); }
-	inline addr_t trunc_page(addr_t addr)    { return addr & _align_mask(get_page_size_log2()); }
-	inline addr_t round_page(addr_t addr)    { return trunc_page(addr + get_page_size() - 1); }
 
+	template <typename T>
+	inline T trunc_page(T addr) { return addr & _align_mask(get_page_size_log2()); }
+
+	template <typename T>
+	inline T round_page(T addr) { return trunc_page(addr + get_page_size() - 1); }
 
 	inline addr_t map_src_addr(addr_t /* core_local */, addr_t phys) { return phys; }
 