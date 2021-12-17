cnuke/genode

sculpt_manager: integrate GPU device access
Similar to other devices allow for selecting a graphics device as
well. This is currently only useful on iMX8-based systems like
iMX8Q-EVK and MNT Reform2 where the driver is explicitly started
from a pkg.

Fixes #4342.

repos/gems/src/app/sculpt_manager/model/runtime_config.h
@@ -355,6 +355,7 @@ class Sculpt::Runtime_config
 				_pci_net   { _r, Type::PLATFORM,    "network hardware", "nic" },
 				_pci_audio { _r, Type::PLATFORM,    "audio hardware",   "audio" },
 				_pci_acpi  { _r, Type::PLATFORM,    "ACPI",             "acpica" },
+				_hw_gpu    { _r, Type::PLATFORM,    "graphics hardware", "gpu" },
 				_trace     { _r, Type::TRACE,       "system-global tracing" },
 				_vm        { _r, Type::VM,          "virtualization hardware" },
 				_pd        { _r, Type::PD,          "system PD service" };

llaa33219/sponge-os

repos/gems/src/app/sculpt_manager/model/runtime_config.h
@@ -355,6 +355,7 @@ class Sculpt::Runtime_config
 				_pci_net   { _r, Type::PLATFORM,    "network hardware", "nic" },
 				_pci_audio { _r, Type::PLATFORM,    "audio hardware",   "audio" },
 				_pci_acpi  { _r, Type::PLATFORM,    "ACPI",             "acpica" },
+				_hw_gpu    { _r, Type::PLATFORM,    "GPU hardware",     "gpu" },
 				_trace     { _r, Type::TRACE,       "system-global tracing" },
 				_vm        { _r, Type::VM,          "virtualization hardware" },
 				_pd        { _r, Type::PD,          "system PD service" };