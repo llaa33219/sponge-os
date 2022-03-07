genodelabs/genode

platform: introduce I/O ports in API
* Introduces Platform::Device::Io_port client utility
* Implements I/O ports as common device resources in platform driver

Fix genodelabs/genode#4436

repos/os/include/platform_session/device.h
@@ -21,6 +21,7 @@
 #include <base/exception.h>
 #include <io_mem_session/client.h>
 #include <irq_session/client.h>
+#include <io_port_session/client.h>
 #include <platform_session/connection.h>
 
 class Platform::Device : Interface, Noncopyable
@@ -29,6 +30,7 @@ class Platform::Device : Interface, Noncopyable
 
 		struct Mmio;
 		struct Irq;
+		struct Io_port_range;
 
 		typedef Platform::Session::Device_name Name;
 
@@ -50,6 +52,11 @@ class Platform::Device : Interface, Noncopyable
 		Io_mem_session_capability _io_mem(unsigned index, Range &range, Cache cache)
 		{
 			return _cap.call<Device_interface::Rpc_io_mem>(index, range, cache);
+		}
+
+		Io_port_session_capability _io_port_range(unsigned index)
+		{
+			return _cap.call<Device_interface::Rpc_io_port_range>(index);
 		}
 
 		Region_map &_rm() { return _platform._rm; }
@@ -165,6 +172,35 @@ class Platform::Device::Irq : Noncopyable
 		{
 			_irq.sigh(sigh);
 		}
+};
+
+
+class Platform::Device::Io_port_range : Noncopyable
+{
+	private:
+
+		Io_port_session_client _io_port_range;
+
+	public:
+
+		struct Index  { unsigned value; };
+
+		Io_port_range(Device &device, Index index)
+		: _io_port_range(device._io_port_range(index.value)) { }
+
+		explicit Io_port_range(Device &device)
+		: Io_port_range(device, Index { 0 }) { }
+
+		uint8_t  inb(uint16_t off) { return _io_port_range.inb(off); };
+		uint16_t inw(uint16_t off) { return _io_port_range.inw(off); };
+		uint32_t inl(uint16_t off) { return _io_port_range.inl(off); };
+
+		void outb(uint16_t off, uint8_t  value) {
+			_io_port_range.outb(off, value); };
+		void outw(uint16_t off, uint16_t value) {
+			_io_port_range.outw(off, value); };
+		void outl(uint16_t off, uint32_t value) {
+			_io_port_range.outl(off, value); };
 };
 
 #endif /* _INCLUDE__PLATFORM_SESSION__DEVICE_H_ */

llaa33219/sponge-os

repos/os/include/platform_session/device.h
@@ -21,6 +21,7 @@
 #include <base/exception.h>
 #include <io_mem_session/client.h>
 #include <irq_session/client.h>
+#include <io_port_session/client.h>
 #include <platform_session/connection.h>
 
 class Platform::Device : Interface, Noncopyable
@@ -29,6 +30,7 @@ class Platform::Device : Interface, Noncopyable
 
 		struct Mmio;
 		struct Irq;
+		struct Io_port_range;
 
 		typedef Platform::Session::Device_name Name;
 
@@ -50,6 +52,11 @@ class Platform::Device : Interface, Noncopyable
 		Io_mem_session_capability _io_mem(unsigned index, Range &range, Cache cache)
 		{
 			return _cap.call<Device_interface::Rpc_io_mem>(index, range, cache);
+		}
+
+		Io_port_session_capability _io_port_range(unsigned index)
+		{
+			return _cap.call<Device_interface::Rpc_io_port_range>(index);
 		}
 
 		Region_map &_rm() { return _platform._rm; }
@@ -165,6 +172,35 @@ class Platform::Device::Irq : Noncopyable
 		{
 			_irq.sigh(sigh);
 		}
+};
+
+
+class Platform::Device::Io_port_range : Noncopyable
+{
+	private:
+
+		Io_port_session_client _io_port_range;
+
+	public:
+
+		struct Index  { unsigned value; };
+
+		Io_port_range(Device &device, Index index)
+		: _io_port_range(device._io_port_range(index.value)) { }
+
+		explicit Io_port_range(Device &device)
+		: Io_port_range(device, Index { 0 }) { }
+
+		uint8_t  inb(uint16_t addr) { return _io_port_range.inb(addr); };
+		uint16_t inw(uint16_t addr) { return _io_port_range.inw(addr); };
+		uint32_t inl(uint16_t addr) { return _io_port_range.inl(addr); };
+
+		void outb(uint16_t addr, uint8_t  value) {
+			_io_port_range.outb(addr, value); };
+		void outw(uint16_t addr, uint16_t value) {
+			_io_port_range.outw(addr, value); };
+		void outl(uint16_t addr, uint32_t value) {
+			_io_port_range.outl(addr, value); };
 };
 
 #endif /* _INCLUDE__PLATFORM_SESSION__DEVICE_H_ */