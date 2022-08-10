jschlatow/genode

nic_router: relax timer accuracy for timestamps
The NIC router uses the timer for relatively coarse-grained timeouts.
It therefore suffices to update and store the current time when the NIC router
is signalled and use the cached time instead. This prevents frequent
syscalls or RPCs when acquiring the current time for every packet.

genodelabs/genode#4555

repos/os/src/server/nic_router/dhcp_client.h
@@ -14,8 +14,10 @@
 #ifndef _DHCP_CLIENT_H_
 #define _DHCP_CLIENT_H_
 
+/* local includes */
+#include <timer.h>
+
 /* Genode includes */
-#include <timer_session/connection.h>
 #include <net/dhcp.h>
 
 namespace Net {
@@ -60,7 +62,7 @@ class Net::Dhcp_client
 
 	public:
 
-		Dhcp_client(Timer::Connection &timer,
+		Dhcp_client(Cached_timer      &timer,
 		            Interface         &interface);
 
 		void handle_dhcp_reply(Dhcp_packet &dhcp);

repos/os/src/server/nic_router/dhcp_server.h
@@ -20,6 +20,7 @@
 #include <pointer.h>
 #include <dns.h>
 #include <ipv4_config.h>
+#include <timer.h>
 
 /* Genode includes */
 #include <net/mac_address.h>
@@ -171,7 +172,7 @@ class Net::Dhcp_allocation : public  Genode::Avl_node<Dhcp_allocation>,
 		Dhcp_allocation(Interface            &interface,
 		                Ipv4_address   const &ip,
 		                Mac_address    const &mac,
-		                Timer::Connection    &timer,
+		                Cached_timer         &timer,
 		                Genode::Microseconds  lifetime);
 
 		~Dhcp_allocation();

repos/os/src/server/nic_router/interface.cc
@@ -1531,6 +1531,7 @@ void Interface::_handle_pkt()
 
 void Interface::_handle_pkt_stream_signal()
 {
+	_timer.update_time();
 	/*
 	 * Release all sent packets that were already acknowledged by the counter
 	 * side. Doing this first frees packet-stream memory which facilitates
@@ -1821,7 +1822,7 @@ void Interface::_send_submit_pkt(Packet_descriptor &pkt,
 
 
 Interface::Interface(Genode::Entrypoint     &ep,
-                     Timer::Connection      &timer,
+                     Cached_timer           &timer,
                      Mac_address      const  router_mac,
                      Genode::Allocator      &alloc,
                      Mac_address      const  mac,

repos/os/src/server/nic_router/main.cc
@@ -21,6 +21,7 @@
 #include <nic_session_root.h>
 #include <uplink_session_root.h>
 #include <configuration.h>
+#include <timer.h>
 
 using namespace Net;
 using namespace Genode;
@@ -35,7 +36,7 @@ class Net::Main
 		Genode::Env                    &_env;
 		Quota                           _shared_quota        { };
 		Interface_list                  _interfaces          { };
-		Timer::Connection               _timer               { _env };
+		Cached_timer                    _timer               { _env };
 		Genode::Heap                    _heap                { &_env.ram(), &_env.rm() };
 		Genode::Attached_rom_dataspace  _config_rom          { _env, "config" };
 		Reference<Configuration>        _config              { *new (_heap) Configuration { _config_rom.xml(), _heap } };

repos/os/src/server/nic_router/report.h
@@ -14,8 +14,10 @@
 #ifndef _REPORT_H_
 #define _REPORT_H_
 
+/* local includes */
+#include <timer.h>
+
 /* Genode */
-#include <timer_session/connection.h>
 #include <os/reporter.h>
 
 namespace Genode {
@@ -68,7 +70,7 @@ class Net::Report
 
 		Report(bool             const &verbose,
 		       Genode::Xml_node const  node,
-		       Timer::Connection      &timer,
+		       Cached_timer           &timer,
 		       Domain_tree            &domains,
 		       Quota            const &shared_quota,
 		       Genode::Pd_session     &pd,

llaa33219/sponge-os

repos/os/src/server/nic_router/dhcp_client.h
@@ -14,8 +14,10 @@
 #ifndef _DHCP_CLIENT_H_
 #define _DHCP_CLIENT_H_
 
+/* local includes */
+#include <cached_timer.h>
+
 /* Genode includes */
-#include <timer_session/connection.h>
 #include <net/dhcp.h>
 
 namespace Net {
@@ -60,7 +62,7 @@ class Net::Dhcp_client
 
 	public:
 
-		Dhcp_client(Timer::Connection &timer,
+		Dhcp_client(Cached_timer      &timer,
 		            Interface         &interface);
 
 		void handle_dhcp_reply(Dhcp_packet &dhcp);

repos/os/src/server/nic_router/dhcp_server.h
@@ -20,6 +20,7 @@
 #include <pointer.h>
 #include <dns.h>
 #include <ipv4_config.h>
+#include <cached_timer.h>
 
 /* Genode includes */
 #include <net/mac_address.h>
@@ -171,7 +172,7 @@ class Net::Dhcp_allocation : public  Genode::Avl_node<Dhcp_allocation>,
 		Dhcp_allocation(Interface            &interface,
 		                Ipv4_address   const &ip,
 		                Mac_address    const &mac,
-		                Timer::Connection    &timer,
+		                Cached_timer         &timer,
 		                Genode::Microseconds  lifetime);
 
 		~Dhcp_allocation();

repos/os/src/server/nic_router/interface.cc
@@ -1532,6 +1532,8 @@ void Interface::_handle_pkt()
 
 void Interface::_handle_pkt_stream_signal()
 {
+	_timer.update_cached_time();
+
 	/*
 	 * Release all sent packets that were already acknowledged by the counter
 	 * side. Doing this first frees packet-stream memory which facilitates
@@ -1832,7 +1834,7 @@ void Interface::_send_submit_pkt(Packet_descriptor &pkt,
 
 
 Interface::Interface(Genode::Entrypoint     &ep,
-                     Timer::Connection      &timer,
+                     Cached_timer           &timer,
                      Mac_address      const  router_mac,
                      Genode::Allocator      &alloc,
                      Mac_address      const  mac,

repos/os/src/server/nic_router/main.cc
@@ -21,6 +21,7 @@
 #include <nic_session_root.h>
 #include <uplink_session_root.h>
 #include <configuration.h>
+#include <cached_timer.h>
 
 using namespace Net;
 using namespace Genode;
@@ -35,7 +36,7 @@ class Net::Main
 		Genode::Env                    &_env;
 		Quota                           _shared_quota        { };
 		Interface_list                  _interfaces          { };
-		Timer::Connection               _timer               { _env };
+		Cached_timer                    _timer               { _env };
 		Genode::Heap                    _heap                { &_env.ram(), &_env.rm() };
 		Genode::Attached_rom_dataspace  _config_rom          { _env, "config" };
 		Reference<Configuration>        _config              { *new (_heap) Configuration { _config_rom.xml(), _heap } };

repos/os/src/server/nic_router/report.h
@@ -14,8 +14,10 @@
 #ifndef _REPORT_H_
 #define _REPORT_H_
 
+/* local includes */
+#include <cached_timer.h>
+
 /* Genode */
-#include <timer_session/connection.h>
 #include <os/reporter.h>
 
 namespace Genode {
@@ -68,7 +70,7 @@ class Net::Report
 
 		Report(bool             const &verbose,
 		       Genode::Xml_node const  node,
-		       Timer::Connection      &timer,
+		       Cached_timer           &timer,
 		       Domain_tree            &domains,
 		       Quota            const &shared_quota,
 		       Genode::Pd_session     &pd,