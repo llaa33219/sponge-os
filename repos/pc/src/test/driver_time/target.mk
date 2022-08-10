ssumpf/genode

pc: add linux driver timer test
The test runs as lx_user task and uses several *delay and wait queue test
cases happened to be used in real ported linux drivers. The test shows
the time spent with several time sources, e.g. jiffies, rdtsc,
lx_time_counter_count etc.

Issue #4540

repos/pc/src/test/driver_time/target.mk
@@ -1,0 +1,28 @@
+REQUIRES := x86_x64
+
+TARGET  := test-driver_time
+LIBS    := base pc_lx_emul jitterentropy
+
+SRC_CC  += main.cc time.cc
+SRC_C   += lx_user.c
+SRC_C   += dummies.c
+SRC_C   += generated_dummies.c
+
+SRC_C   += lx_emul/common_dummies.c
+SRC_C   += lx_emul/shadow/lib/kobject_uevent.c
+SRC_C   += lx_emul/shadow/drivers/char/random.c
+SRC_C   += lx_emul/shadow/kernel/softirq.c
+
+vpath %.c  $(REP_DIR)/src/lib/pc
+vpath %.cc $(REP_DIR)/src/lib/pc
+
+LX_SRC_DIR := $(call select_from_ports,linux)/src/linux
+ifeq ($(wildcard $(LX_SRC_DIR)),)
+LX_SRC_DIR := $(call select_from_repositories,src/linux)
+endif
+ifeq ($(wildcard $(LX_SRC_DIR)),)
+fail
+endif
+
+INC_DIR += $(PRG_DIR)
+INC_DIR += $(LX_SRC_DIR)/drivers/gpu/drm/i915

llaa33219/sponge-os

repos/pc/src/test/driver_time/target.mk
@@ -1,0 +1,28 @@
+REQUIRES := x86_64
+
+TARGET  := test-driver_time
+LIBS    := base pc_lx_emul jitterentropy
+
+SRC_CC  += main.cc time.cc
+SRC_C   += lx_user.c
+SRC_C   += dummies.c
+SRC_C   += generated_dummies.c
+
+SRC_C   += lx_emul/common_dummies.c
+SRC_C   += lx_emul/shadow/lib/kobject_uevent.c
+SRC_C   += lx_emul/shadow/drivers/char/random.c
+SRC_C   += lx_emul/shadow/kernel/softirq.c
+
+vpath %.c  $(REP_DIR)/src/lib/pc
+vpath %.cc $(REP_DIR)/src/lib/pc
+
+LX_SRC_DIR := $(call select_from_ports,linux)/src/linux
+ifeq ($(wildcard $(LX_SRC_DIR)),)
+LX_SRC_DIR := $(call select_from_repositories,src/linux)
+endif
+ifeq ($(wildcard $(LX_SRC_DIR)),)
+fail
+endif
+
+INC_DIR += $(PRG_DIR)
+INC_DIR += $(LX_SRC_DIR)/drivers/gpu/drm/i915