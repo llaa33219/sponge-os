ssumpf/genode

nic_router: use the dictionary data structure
This commit gets rid of the router-local wrapper of Genode's AVL string tree
and replaces it with Genode's new Dictionary structure. The Dictionary is now
used for managing domains and NIC clients. Due to this change, the formerly
necessary helper classes Domain_base and Nic_client_base could be removed as
well.

Ref #4610

repos/os/src/server/nic_router/configuration.cc
@@ -50,24 +50,12 @@ Configuration::Configuration(Xml_node const  node,
 { }
 
 
-void Configuration::_invalid_nic_client(Nic_client &nic_client,
-                                        char const *reason)
-{
-	if (_verbose) {
-		log("[", nic_client.domain(), "] invalid NIC client: ", nic_client, " (", reason, ")"); }
-
-	_nic_clients.remove(nic_client);
-	destroy(_alloc, &nic_client);
-}
-
-
 void Configuration::_invalid_domain(Domain     &domain,
                                     char const *reason)
 {
 	if (_verbose) {
 		log("[", domain, "] invalid domain (", reason, ") "); }
 
-	_domains.remove(domain);
 	destroy(_alloc, &domain);
 }
 
@@ -133,13 +121,27 @@ Configuration::Configuration(Env                             &env,
 	/* do parts of domain initialization that do not lookup other domains */
 	node.for_each_sub_node("domain", [&] (Xml_node const node) {
 		try {
-			Domain &domain = *new (_alloc) Domain(*this, node, _alloc);
-			_domains.insert(
-				domain,
-				[&] /* handle_name_not_unique */ (Domain &other_domain)
+			Domain_name const name {
+				node.attribute_value("name", Domain_name { }) };
+
+			_domains.apply(
+				name,
+				[&] /* match_fn */ (Domain &other_domain)
 				{
-					_invalid_domain(domain,       "name not unique");
-					_invalid_domain(other_domain, "name not unique");
+					if (_verbose) {
+
+						log("[", name,
+						    "] invalid domain (name not unique) ");
+
+						log("[", other_domain,
+						    "] invalid domain (name not unique) ");
+					}
+					destroy(_alloc, &other_domain);
+				},
+				[&] /* no_match_fn */ ()
+				{
+					new (_alloc) Domain {
+						*this, node, name, _alloc, _domains };
 				}
 			);
 		}
@@ -165,7 +167,6 @@ Configuration::Configuration(Env                             &env,
 		catch (Retry_without_domain exception) {
 
 			/* destroy domain that became invalid during initialization */
-			_domains.remove(exception.domain);
 			destroy(_alloc, &exception.domain);
 
 			/* deinitialize the remaining domains again */
@@ -203,24 +204,39 @@ Configuration::Configuration(Env                             &env,
 	/* initialize NIC clients */
 	_node.for_each_sub_node("nic-client", [&] (Xml_node const node) {
 		try {
-			Nic_client &nic_client = *new (_alloc)
-				Nic_client { node, alloc, old_config._nic_clients, env, timer,
-				             interfaces, *this };
+			Session_label const label {
+				node.attribute_value("label",  Session_label::String { }) };
 
-			_nic_clients.insert(
-				nic_client,
-				[&] /* handle_name_not_unique */ (Nic_client &other_nic_client)
+			Domain_name const domain {
+				node.attribute_value("domain", Domain_name { }) };
+
+			_nic_clients.apply(
+				label,
+				[&] /* match */ (Nic_client &nic_client)
 				{
-					_invalid_nic_client(nic_client,       "label not unique");
-					_invalid_nic_client(other_nic_client, "label not unique");
+					if (_verbose) {
+
+						log("[", domain, "] invalid NIC client: ",
+						    label, " (label not unique)");
+
+						log("[", nic_client.domain(), "] invalid NIC client: ",
+						    nic_client.label(), " (label not unique)");
+					}
+					destroy(_alloc, &nic_client);
+				},
+				[&] /* no_match */ ()
+				{
+					new (_alloc) Nic_client {
+						label, domain, alloc, old_config._nic_clients,
+						_nic_clients, env, timer, interfaces, *this };
 				}
 			);
 		}
 		catch (Nic_client::Invalid) { }
 	});
 	/*
-	 * Destroy old NIC clients to ensure that NIC client interfaces that were not
-	 * re-used are not re-attached to the new domains.
+	 * Destroy old NIC clients to ensure that NIC client interfaces that were
+	 * not re-used are not re-attached to the new domains.
 	 */
 	old_config._nic_clients.destroy_each(_alloc);
 }

repos/os/src/server/nic_router/interface.cc
@@ -386,9 +386,9 @@ void Interface::_update_domain_object(Domain &new_domain) {
 
 void Interface::attach_to_domain()
 {
-	_config().domains().find_by_name(
+	_config().domains().apply(
 		_policy.determine_domain_name(),
-		[&] /* handle_match */ (Domain &domain)
+		[&] /* match_fn */ (Domain &domain)
 		{
 			_attach_to_domain_raw(domain);
 
@@ -398,7 +398,7 @@ void Interface::attach_to_domain()
 			}
 			attach_to_domain_finish();
 		},
-		[&] /* handle_no_match */ () { }
+		[&] /* no_match_fn */ () { }
 	);
 }
 
@@ -2158,9 +2158,9 @@ void Interface::_update_own_arp_waiters(Domain &domain)
 	{
 		Arp_waiter &arp_waiter { *le.object() };
 		bool dismiss_arp_waiter { true };
-		_config().domains().find_by_name(
+		_config().domains().apply(
 			arp_waiter.dst().name(),
-			[&] /* handle_match */ (Domain &dst)
+			[&] /* match_fn */ (Domain &dst)
 			{
 				/* dismiss ARP waiter if IP config of target domain changed */
 				if (dst.ip_config() != arp_waiter.dst().ip_config()) {
@@ -2173,7 +2173,7 @@ void Interface::_update_own_arp_waiters(Domain &domain)
 				}
 				dismiss_arp_waiter = false;
 			},
-			[&] /* handle_no_match */ ()
+			[&] /* no_match_fn */ ()
 			{
 				/* dismiss ARP waiter as the target domain disappeared */
 			}
@@ -2207,13 +2207,13 @@ void Interface::handle_config_1(Configuration &config)
 			return; }
 
 		/* interface stays with its domain, so, try to reuse IP config */
-		config.domains().find_by_name(
+		config.domains().apply(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				new_domain.try_reuse_ip_config(old_domain);
 			},
-			[&] /* handle_no_match */ () { }
+			[&] /* no_match_fn */ () { }
 		);
 	}
 	catch (Pointer<Domain>::Invalid) { }
@@ -2246,9 +2246,9 @@ void Interface::handle_config_2()
 	Domain_name const &new_domain_name = _policy.determine_domain_name();
 	try {
 		Domain &old_domain = domain();
-		_config().domains().find_by_name(
+		_config().domains().apply(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				/* if the domains differ, detach completely from the domain */
 				if (old_domain.name() != new_domain_name) {
@@ -2282,7 +2282,7 @@ void Interface::handle_config_2()
 				/* remember that the interface stays attached to the same domain */
 				_update_domain.construct(old_domain, new_domain);
 			},
-			[&] /* handle_no_match */ ()
+			[&] /* no_match_fn */ ()
 			{
 				/* the interface no longer has a domain */
 				_detach_from_domain();
@@ -2297,9 +2297,9 @@ void Interface::handle_config_2()
 	catch (Pointer<Domain>::Invalid) {
 
 		/* the interface had no domain but now it may get one */
-		_config().domains().find_by_name(
+		_config().domains().apply(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				_attach_to_domain_raw(new_domain);
 
@@ -2308,7 +2308,7 @@ void Interface::handle_config_2()
 					_dhcp_client.construct(_timer, *this);
 				}
 			},
-			[&] /* handle_no_match */ () { }
+			[&] /* no_match_fn */ () { }
 		);
 	}
 }

repos/os/src/server/nic_router/nic_client.cc
@@ -22,17 +22,6 @@ using namespace Net;
 using namespace Genode;
 
 
-/*********************
- ** Nic_client_base **
- *********************/
-
-Net::Nic_client_base::Nic_client_base(Xml_node const &node)
-:
-	_label  { node.attribute_value("label",  Session_label::String()) },
-	_domain { node.attribute_value("domain", Domain_name()) }
-{ }
-
-
 /****************
  ** Nic_client **
  ****************/
@@ -40,26 +29,29 @@ Net::Nic_client_base::Nic_client_base(Xml_node const &node)
 void Nic_client::_invalid(char const *reason) const
 {
 	if (_config.verbose()) {
-		log("[", domain(), "] invalid NIC client: ", *this, " (", reason, ")"); }
-
+		log("[", domain(), "] invalid NIC client: ", label(),
+		    " (", reason, ")");
+	}
 	throw Invalid();
 }
 
 
-Net::Nic_client::Nic_client(Xml_node    const &node,
-                            Allocator         &alloc,
-                            Nic_client_tree   &old_nic_clients,
-                            Env               &env,
-                            Cached_timer      &timer,
-                            Interface_list    &interfaces,
-                            Configuration     &config)
+Net::Nic_client::Nic_client(Session_label const &label_arg,
+                            Domain_name   const &domain_arg,
+                            Allocator           &alloc,
+                            Nic_client_dict     &old_nic_clients,
+                            Nic_client_dict     &new_nic_clients,
+                            Env                 &env,
+                            Cached_timer        &timer,
+                            Interface_list      &interfaces,
+                            Configuration       &config)
 :
-	Nic_client_base { node },
-	Avl_string_base { label().string() },
-	_alloc          { alloc },
-	_config         { config }
+	Nic_client_dict::Element { new_nic_clients, label_arg },
+	_alloc                   { alloc },
+	_config                  { config },
+	_domain                  { domain_arg }
 {
-	old_nic_clients.find_by_name(
+	old_nic_clients.apply(
 		label(),
 		[&] /* handle_match */ (Nic_client &old_nic_client)
 		{
@@ -73,7 +65,7 @@ Net::Nic_client::Nic_client(Xml_node    const &node,
 		{
 			/* create a new interface */
 			if (config.verbose()) {
-				log("[", domain(), "] create NIC client: ", *this); }
+				log("[", domain(), "] create NIC client: ", label()); }
 
 			try {
 				_interface = *new (_alloc)
@@ -95,20 +87,11 @@ Net::Nic_client::~Nic_client()
 	try {
 		Nic_client_interface &interface = _interface();
 		if (_config.verbose()) {
-			log("[", domain(), "] destroy NIC client: ", *this); }
+			log("[", domain(), "] destroy NIC client: ", label()); }
 
 		destroy(_alloc, &interface);
 	}
 	catch (Pointer<Nic_client_interface>::Invalid) { }
-}
-
-
-void Net::Nic_client::print(Output &output) const
-{
-	if (label() == Session_label()) {
-		Genode::print(output, "?"); }
-	else {
-		Genode::print(output, label()); }
 }
 
 

llaa33219/sponge-os

repos/os/src/server/nic_router/configuration.cc
@@ -50,24 +50,12 @@ Configuration::Configuration(Xml_node const  node,
 { }
 
 
-void Configuration::_invalid_nic_client(Nic_client &nic_client,
-                                        char const *reason)
-{
-	if (_verbose) {
-		log("[", nic_client.domain(), "] invalid NIC client: ", nic_client, " (", reason, ")"); }
-
-	_nic_clients.remove(nic_client);
-	destroy(_alloc, &nic_client);
-}
-
-
 void Configuration::_invalid_domain(Domain     &domain,
                                     char const *reason)
 {
 	if (_verbose) {
 		log("[", domain, "] invalid domain (", reason, ") "); }
 
-	_domains.remove(domain);
 	destroy(_alloc, &domain);
 }
 
@@ -133,13 +121,27 @@ Configuration::Configuration(Env                             &env,
 	/* do parts of domain initialization that do not lookup other domains */
 	node.for_each_sub_node("domain", [&] (Xml_node const node) {
 		try {
-			Domain &domain = *new (_alloc) Domain(*this, node, _alloc);
-			_domains.insert(
-				domain,
-				[&] /* handle_name_not_unique */ (Domain &other_domain)
+			Domain_name const name {
+				node.attribute_value("name", Domain_name { }) };
+
+			_domains.with_element(
+				name,
+				[&] /* match_fn */ (Domain &other_domain)
 				{
-					_invalid_domain(domain,       "name not unique");
-					_invalid_domain(other_domain, "name not unique");
+					if (_verbose) {
+
+						log("[", name,
+						    "] invalid domain (name not unique) ");
+
+						log("[", other_domain,
+						    "] invalid domain (name not unique) ");
+					}
+					destroy(_alloc, &other_domain);
+				},
+				[&] /* no_match_fn */ ()
+				{
+					new (_alloc) Domain {
+						*this, node, name, _alloc, _domains };
 				}
 			);
 		}
@@ -165,7 +167,6 @@ Configuration::Configuration(Env                             &env,
 		catch (Retry_without_domain exception) {
 
 			/* destroy domain that became invalid during initialization */
-			_domains.remove(exception.domain);
 			destroy(_alloc, &exception.domain);
 
 			/* deinitialize the remaining domains again */
@@ -203,24 +204,39 @@ Configuration::Configuration(Env                             &env,
 	/* initialize NIC clients */
 	_node.for_each_sub_node("nic-client", [&] (Xml_node const node) {
 		try {
-			Nic_client &nic_client = *new (_alloc)
-				Nic_client { node, alloc, old_config._nic_clients, env, timer,
-				             interfaces, *this };
+			Session_label const label {
+				node.attribute_value("label",  Session_label::String { }) };
 
-			_nic_clients.insert(
-				nic_client,
-				[&] /* handle_name_not_unique */ (Nic_client &other_nic_client)
+			Domain_name const domain {
+				node.attribute_value("domain", Domain_name { }) };
+
+			_nic_clients.with_element(
+				label,
+				[&] /* match */ (Nic_client &nic_client)
 				{
-					_invalid_nic_client(nic_client,       "label not unique");
-					_invalid_nic_client(other_nic_client, "label not unique");
+					if (_verbose) {
+
+						log("[", domain, "] invalid NIC client: ",
+						    label, " (label not unique)");
+
+						log("[", nic_client.domain(), "] invalid NIC client: ",
+						    nic_client.label(), " (label not unique)");
+					}
+					destroy(_alloc, &nic_client);
+				},
+				[&] /* no_match */ ()
+				{
+					new (_alloc) Nic_client {
+						label, domain, alloc, old_config._nic_clients,
+						_nic_clients, env, timer, interfaces, *this };
 				}
 			);
 		}
 		catch (Nic_client::Invalid) { }
 	});
 	/*
-	 * Destroy old NIC clients to ensure that NIC client interfaces that were not
-	 * re-used are not re-attached to the new domains.
+	 * Destroy old NIC clients to ensure that NIC client interfaces that were
+	 * not re-used are not re-attached to the new domains.
 	 */
 	old_config._nic_clients.destroy_each(_alloc);
 }

repos/os/src/server/nic_router/interface.cc
@@ -386,9 +386,9 @@ void Interface::_update_domain_object(Domain &new_domain) {
 
 void Interface::attach_to_domain()
 {
-	_config().domains().find_by_name(
+	_config().domains().with_element(
 		_policy.determine_domain_name(),
-		[&] /* handle_match */ (Domain &domain)
+		[&] /* match_fn */ (Domain &domain)
 		{
 			_attach_to_domain_raw(domain);
 
@@ -398,7 +398,7 @@ void Interface::attach_to_domain()
 			}
 			attach_to_domain_finish();
 		},
-		[&] /* handle_no_match */ () { }
+		[&] /* no_match_fn */ () { }
 	);
 }
 
@@ -2158,9 +2158,9 @@ void Interface::_update_own_arp_waiters(Domain &domain)
 	{
 		Arp_waiter &arp_waiter { *le.object() };
 		bool dismiss_arp_waiter { true };
-		_config().domains().find_by_name(
+		_config().domains().with_element(
 			arp_waiter.dst().name(),
-			[&] /* handle_match */ (Domain &dst)
+			[&] /* match_fn */ (Domain &dst)
 			{
 				/* dismiss ARP waiter if IP config of target domain changed */
 				if (dst.ip_config() != arp_waiter.dst().ip_config()) {
@@ -2173,7 +2173,7 @@ void Interface::_update_own_arp_waiters(Domain &domain)
 				}
 				dismiss_arp_waiter = false;
 			},
-			[&] /* handle_no_match */ ()
+			[&] /* no_match_fn */ ()
 			{
 				/* dismiss ARP waiter as the target domain disappeared */
 			}
@@ -2207,13 +2207,13 @@ void Interface::handle_config_1(Configuration &config)
 			return; }
 
 		/* interface stays with its domain, so, try to reuse IP config */
-		config.domains().find_by_name(
+		config.domains().with_element(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				new_domain.try_reuse_ip_config(old_domain);
 			},
-			[&] /* handle_no_match */ () { }
+			[&] /* no_match_fn */ () { }
 		);
 	}
 	catch (Pointer<Domain>::Invalid) { }
@@ -2246,9 +2246,9 @@ void Interface::handle_config_2()
 	Domain_name const &new_domain_name = _policy.determine_domain_name();
 	try {
 		Domain &old_domain = domain();
-		_config().domains().find_by_name(
+		_config().domains().with_element(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				/* if the domains differ, detach completely from the domain */
 				if (old_domain.name() != new_domain_name) {
@@ -2282,7 +2282,7 @@ void Interface::handle_config_2()
 				/* remember that the interface stays attached to the same domain */
 				_update_domain.construct(old_domain, new_domain);
 			},
-			[&] /* handle_no_match */ ()
+			[&] /* no_match_fn */ ()
 			{
 				/* the interface no longer has a domain */
 				_detach_from_domain();
@@ -2297,9 +2297,9 @@ void Interface::handle_config_2()
 	catch (Pointer<Domain>::Invalid) {
 
 		/* the interface had no domain but now it may get one */
-		_config().domains().find_by_name(
+		_config().domains().with_element(
 			new_domain_name,
-			[&] /* handle_match */ (Domain &new_domain)
+			[&] /* match_fn */ (Domain &new_domain)
 			{
 				_attach_to_domain_raw(new_domain);
 
@@ -2308,7 +2308,7 @@ void Interface::handle_config_2()
 					_dhcp_client.construct(_timer, *this);
 				}
 			},
-			[&] /* handle_no_match */ () { }
+			[&] /* no_match_fn */ () { }
 		);
 	}
 }

repos/os/src/server/nic_router/nic_client.cc
@@ -22,17 +22,6 @@ using namespace Net;
 using namespace Genode;
 
 
-/*********************
- ** Nic_client_base **
- *********************/
-
-Net::Nic_client_base::Nic_client_base(Xml_node const &node)
-:
-	_label  { node.attribute_value("label",  Session_label::String()) },
-	_domain { node.attribute_value("domain", Domain_name()) }
-{ }
-
-
 /****************
  ** Nic_client **
  ****************/
@@ -40,26 +29,29 @@ Net::Nic_client_base::Nic_client_base(Xml_node const &node)
 void Nic_client::_invalid(char const *reason) const
 {
 	if (_config.verbose()) {
-		log("[", domain(), "] invalid NIC client: ", *this, " (", reason, ")"); }
-
+		log("[", domain(), "] invalid NIC client: ", label(),
+		    " (", reason, ")");
+	}
 	throw Invalid();
 }
 
 
-Net::Nic_client::Nic_client(Xml_node    const &node,
-                            Allocator         &alloc,
-                            Nic_client_tree   &old_nic_clients,
-                            Env               &env,
-                            Cached_timer      &timer,
-                            Interface_list    &interfaces,
-                            Configuration     &config)
+Net::Nic_client::Nic_client(Session_label const &label_arg,
+                            Domain_name   const &domain_arg,
+                            Allocator           &alloc,
+                            Nic_client_dict     &old_nic_clients,
+                            Nic_client_dict     &new_nic_clients,
+                            Env                 &env,
+                            Cached_timer        &timer,
+                            Interface_list      &interfaces,
+                            Configuration       &config)
 :
-	Nic_client_base { node },
-	Avl_string_base { label().string() },
-	_alloc          { alloc },
-	_config         { config }
+	Nic_client_dict::Element { new_nic_clients, label_arg },
+	_alloc                   { alloc },
+	_config                  { config },
+	_domain                  { domain_arg }
 {
-	old_nic_clients.find_by_name(
+	old_nic_clients.with_element(
 		label(),
 		[&] /* handle_match */ (Nic_client &old_nic_client)
 		{
@@ -73,7 +65,7 @@ Net::Nic_client::Nic_client(Xml_node    const &node,
 		{
 			/* create a new interface */
 			if (config.verbose()) {
-				log("[", domain(), "] create NIC client: ", *this); }
+				log("[", domain(), "] create NIC client: ", label()); }
 
 			try {
 				_interface = *new (_alloc)
@@ -95,20 +87,11 @@ Net::Nic_client::~Nic_client()
 	try {
 		Nic_client_interface &interface = _interface();
 		if (_config.verbose()) {
-			log("[", domain(), "] destroy NIC client: ", *this); }
+			log("[", domain(), "] destroy NIC client: ", label()); }
 
 		destroy(_alloc, &interface);
 	}
 	catch (Pointer<Nic_client_interface>::Invalid) { }
-}
-
-
-void Net::Nic_client::print(Output &output) const
-{
-	if (label() == Session_label()) {
-		Genode::print(output, "?"); }
-	else {
-		Genode::print(output, label()); }
 }
 
 