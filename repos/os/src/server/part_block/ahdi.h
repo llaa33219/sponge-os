chelmuth/genode

part_block: modernize implementation
Fixes #4790

repos/os/src/server/part_block/ahdi.h
@@ -1,6 +1,7 @@
 /*
  * \brief  Atari ST partition scheme (AHDI)
  * \author Norman Feske
+ * \author Christian Helmuth
  * \date   2019-08-09
  */
 
@@ -16,84 +17,190 @@
 
 #include "partition_table.h"
 
+namespace Block {
+	struct Ahdi_partition;
+	struct Ahdi;
+}
 
-struct Ahdi
+
+struct Block::Ahdi_partition : Partition
 {
-	typedef Block::Partition_table::Sector Sector;
+	using Type = String<4>;
+	Type type;
 
-	typedef Genode::uint32_t uint32_t;
-	typedef Genode::uint8_t  uint8_t;
+	Ahdi_partition(block_number_t lba,
+	               block_count_t  sectors,
+	               Fs::Type       fs_type,
+	               Type    const &type)
+	:
+		Partition(lba, sectors, fs_type),
+		type(type)
+	{ }
+};
 
-	/**
-	 * 32-bit big-endian value
-	 */
-	struct Be32
-	{
-		uint8_t b0, b1, b2, b3;
 
-		uint32_t value() const {
-			return ((uint32_t)b0 << 24) | ((uint32_t)b1 << 16)
-			     | ((uint32_t)b2 <<  8) | ((uint32_t)b3 <<  0); }
+class Block::Ahdi : public Partition_table
+{
+	private:
 
-	} __attribute__((packed));
-
-	struct Partition_record
-	{
-		uint8_t _flags;
-		uint8_t _id0, _id1, _id2;
-		Be32    start;  /* first block */
-		Be32    length; /* in blocks */
-
-		typedef Genode::String<4> Id;
-
-		Id id() const
+		/**
+		 * 32-bit big-endian value
+		 */
+		struct Be32
 		{
-			using Genode::Char;
-			return Id(Char(_id0), Char(_id1), Char(_id2));
+			uint8_t b0, b1, b2, b3;
+
+			uint32_t value() const {
+				return ((uint32_t)b0 << 24) | ((uint32_t)b1 << 16)
+				     | ((uint32_t)b2 <<  8) | ((uint32_t)b3 <<  0); }
+
+		} __attribute__((packed));
+
+		struct Partition_record
+		{
+			uint8_t _flags;
+			uint8_t _id0, _id1, _id2;
+			Be32    start;  /* first block */
+			Be32    length; /* in blocks */
+
+			Ahdi_partition::Type id() const
+			{
+				return { Char(_id0), Char(_id1), Char(_id2) };
+			}
+
+			bool bootable() const { return _flags & 1; }
+
+			bool valid() const
+			{
+				return start.value() > 0
+				    && (id() == "BGM" || id() == "GEM" || id() == "LNX");
+			}
+
+		} __attribute__((packed));
+
+		enum { MAX_PARTITIONS = 4 };
+
+		struct Root_sector
+		{
+			uint8_t          boot_code[0x156];
+			Partition_record icd_partitions[8];
+			uint8_t          unused[0xc];
+			Be32             disk_blocks;
+			Partition_record partitions[MAX_PARTITIONS];
+
+		} __attribute__((packed));
+
+		Constructible<Ahdi_partition> _part_list[MAX_PARTITIONS];
+
+		bool _valid(Sync_read const &sector)
+		{
+			bool any_partition_valid = false;
+
+			Root_sector const root = *sector.addr<Root_sector const *>();
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++)
+				if (root.partitions[i].valid())
+					any_partition_valid = true;
+
+			return any_partition_valid;
 		}
 
-		bool bootable() const { return _flags & 1; }
+		template <typename FUNC>
+		void _parse_ahdi(Sync_read const &sector, FUNC const &fn)
+		{
+			Root_sector &root = *sector.addr<Root_sector *>();
 
-		bool valid() const { return id() == "BGM" && start.value() > 0; }
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++) {
+				Partition_record const &part = root.partitions[i];
+				if (!part.valid())
+					continue;
 
-	} __attribute__((packed));
-
-	enum { MAX_PARTITIONS = 4 };
-
-	struct Root_sector
-	{
-		uint8_t          boot_code[0x156];
-		Partition_record icd_partitions[8];
-		uint8_t          unused[0xc];
-		Be32             disk_blocks;
-		Partition_record partitions[MAX_PARTITIONS];
-
-	} __attribute__((packed));
-
-	static bool valid(Sector const &sector)
-	{
-		bool any_partition_valid = false;
-
-		Root_sector const root = *sector.addr<Root_sector const *>();
-		for (unsigned i = 0; i < MAX_PARTITIONS; i++)
-			if (root.partitions[i].valid())
-				any_partition_valid = true;
-
-		return any_partition_valid;
-	}
-
-	template <typename FN>
-	static void for_each_partition(Sector const &sector, FN const &fn)
-	{
-		Root_sector &root = *sector.addr<Root_sector *>();
-
-		for (unsigned i = 0; i < MAX_PARTITIONS; i++) {
-			Partition_record const &part = root.partitions[i];
-			if (part.valid())
-				fn(i + 1, Block::Partition(part.start.value(), part.length.value(),
-				                           Fs::Type(), 0));
+				fn(i, part);
+			}
 		}
-	}
+
+		template <typename FN>
+		void _for_each_valid_partition(FN const &fn) const
+		{
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++)
+				if (_part_list[i].constructed())
+					fn(i);
+		};
+
+	public:
+
+		using Partition_table::Partition_table;
+
+		bool parse()
+		{
+			Sync_read s(_handler, _alloc, 0, 1);
+
+			if (!_valid(s))
+				return false;
+
+			_parse_ahdi(s, [&] (unsigned i, Partition_record const &r) {
+				block_number_t lba    = r.start.value();
+				block_count_t  length = r.length.value();
+
+				Ahdi_partition::Type type = r.id();
+
+				/* probe for known file-system types */
+				enum { BYTES = 4096, };
+				Sync_read fs(_handler, _alloc, lba , BYTES / _info.block_size);
+				Fs::Type const fs_type =
+					Fs::probe(fs.addr<uint8_t*>(), BYTES);
+
+				_part_list[i].construct(lba, length, fs_type, type);
+
+				log("AHDI Partition ", i + 1, ": LBA ", lba, " (", length,
+				    " blocks) type: '", type, "'");
+			});
+
+			return true;
+		}
+
+		bool partition_valid(long num) const override
+		{
+			/* 1-based partition number to 0-based array index */
+			num -= 1;
+
+			if (num < 0 || num >= MAX_PARTITIONS)
+				return false;
+
+			return _part_list[num].constructed();
+		}
+
+		block_number_t partition_lba(long num) const override
+		{
+			return partition_valid(num) ? _part_list[num - 1]->lba : 0;
+		}
+
+		block_count_t partition_sectors(long num) const override
+		{
+			return partition_valid(num) ? _part_list[num - 1]->sectors : 0;
+		}
+
+		void generate_report(Xml_generator &xml) const override
+		{
+			auto gen_partition_attr = [&] (Xml_generator &xml, unsigned i)
+			{
+				Ahdi_partition const &part = *_part_list[i];
+
+				xml.attribute("number",     i + 1);
+				xml.attribute("start",      part.lba);
+				xml.attribute("length",     part.sectors);
+				xml.attribute("block_size", _info.block_size);
+				xml.attribute("type",       part.type);
+
+				if (part.fs_type.valid())
+					xml.attribute("file_system", part.fs_type);
+			};
+
+			xml.attribute("type", "ahdi");
+
+			_for_each_valid_partition([&] (unsigned i) {
+				xml.node("partition", [&] {
+					gen_partition_attr(xml, i); }); });
+		}
 };
 
 #endif /* _PART_BLOCK__AHDI_H_ */

llaa33219/sponge-os

repos/os/src/server/part_block/ahdi.h
@@ -1,6 +1,7 @@
 /*
  * \brief  Atari ST partition scheme (AHDI)
  * \author Norman Feske
+ * \author Christian Helmuth
  * \date   2019-08-09
  */
 
@@ -16,84 +17,184 @@
 
 #include "partition_table.h"
 
+namespace Block {
+	struct Ahdi_partition;
+	struct Ahdi;
+}
 
-struct Ahdi
+
+struct Block::Ahdi_partition : Partition
 {
-	typedef Block::Partition_table::Sector Sector;
+	using Type = String<4>;
+	Type type;
 
-	typedef Genode::uint32_t uint32_t;
-	typedef Genode::uint8_t  uint8_t;
+	Ahdi_partition(block_number_t lba,
+	               block_number_t sectors,
+	               Fs::Type       fs_type,
+	               Type    const &type)
+	:
+		Partition(lba, sectors, fs_type),
+		type(type)
+	{ }
+};
 
-	/**
-	 * 32-bit big-endian value
-	 */
-	struct Be32
-	{
-		uint8_t b0, b1, b2, b3;
 
-		uint32_t value() const {
-			return ((uint32_t)b0 << 24) | ((uint32_t)b1 << 16)
-			     | ((uint32_t)b2 <<  8) | ((uint32_t)b3 <<  0); }
+class Block::Ahdi : public Partition_table
+{
+	private:
 
-	} __attribute__((packed));
-
-	struct Partition_record
-	{
-		uint8_t _flags;
-		uint8_t _id0, _id1, _id2;
-		Be32    start;  /* first block */
-		Be32    length; /* in blocks */
-
-		typedef Genode::String<4> Id;
-
-		Id id() const
+		/**
+		 * 32-bit big-endian value
+		 */
+		struct Be32
 		{
-			using Genode::Char;
-			return Id(Char(_id0), Char(_id1), Char(_id2));
+			uint8_t b0, b1, b2, b3;
+
+			uint32_t value() const {
+				return ((uint32_t)b0 << 24) | ((uint32_t)b1 << 16)
+				     | ((uint32_t)b2 <<  8) | ((uint32_t)b3 <<  0); }
+
+		} __attribute__((packed));
+
+		struct Partition_record
+		{
+			uint8_t _flags;
+			uint8_t _id0, _id1, _id2;
+			Be32    start;  /* first block */
+			Be32    length; /* in blocks */
+
+			Ahdi_partition::Type id() const
+			{
+				return { Char(_id0), Char(_id1), Char(_id2) };
+			}
+
+			bool bootable() const { return _flags & 1; }
+
+			bool valid() const
+			{
+				return start.value() > 0
+				    && (id() == "BGM" || id() == "GEM" || id() == "LNX");
+			}
+
+		} __attribute__((packed));
+
+		enum { MAX_PARTITIONS = 4 };
+
+		struct Root_sector
+		{
+			uint8_t          boot_code[0x156];
+			Partition_record icd_partitions[8];
+			uint8_t          unused[0xc];
+			Be32             disk_blocks;
+			Partition_record partitions[MAX_PARTITIONS];
+
+		} __attribute__((packed));
+
+		Constructible<Ahdi_partition> _part_list[MAX_PARTITIONS];
+
+		bool _valid(Sync_read const &sector)
+		{
+			bool any_partition_valid = false;
+
+			Root_sector const root = *sector.addr<Root_sector const *>();
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++)
+				if (root.partitions[i].valid())
+					any_partition_valid = true;
+
+			return any_partition_valid;
 		}
 
-		bool bootable() const { return _flags & 1; }
+		template <typename FUNC>
+		void _parse_ahdi(Sync_read const &sector, FUNC const &fn)
+		{
+			Root_sector &root = *sector.addr<Root_sector *>();
 
-		bool valid() const { return id() == "BGM" && start.value() > 0; }
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++) {
+				Partition_record const &part = root.partitions[i];
+				if (!part.valid())
+					continue;
 
-	} __attribute__((packed));
-
-	enum { MAX_PARTITIONS = 4 };
-
-	struct Root_sector
-	{
-		uint8_t          boot_code[0x156];
-		Partition_record icd_partitions[8];
-		uint8_t          unused[0xc];
-		Be32             disk_blocks;
-		Partition_record partitions[MAX_PARTITIONS];
-
-	} __attribute__((packed));
-
-	static bool valid(Sector const &sector)
-	{
-		bool any_partition_valid = false;
-
-		Root_sector const root = *sector.addr<Root_sector const *>();
-		for (unsigned i = 0; i < MAX_PARTITIONS; i++)
-			if (root.partitions[i].valid())
-				any_partition_valid = true;
-
-		return any_partition_valid;
-	}
-
-	template <typename FN>
-	static void for_each_partition(Sector const &sector, FN const &fn)
-	{
-		Root_sector &root = *sector.addr<Root_sector *>();
-
-		for (unsigned i = 0; i < MAX_PARTITIONS; i++) {
-			Partition_record const &part = root.partitions[i];
-			if (part.valid())
-				fn(i + 1, Block::Partition(part.start.value(), part.length.value(),
-				                           Fs::Type(), 0));
+				fn(i, part);
+			}
 		}
-	}
+
+		template <typename FN>
+		void _for_each_valid_partition(FN const &fn) const
+		{
+			for (unsigned i = 0; i < MAX_PARTITIONS; i++)
+				if (_part_list[i].constructed())
+					fn(i);
+		};
+
+	public:
+
+		using Partition_table::Partition_table;
+
+		bool parse()
+		{
+			Sync_read s(_handler, _alloc, 0, 1);
+
+			if (!s.success() || !_valid(s))
+				return false;
+
+			_parse_ahdi(s, [&] (unsigned i, Partition_record const &r) {
+				block_number_t lba    = r.start.value();
+				block_number_t length = r.length.value();
+
+				Ahdi_partition::Type type = r.id();
+
+				_part_list[i].construct(lba, length, _fs_type(lba), type);
+
+				log("AHDI Partition ", i + 1, ": LBA ", lba, " (", length,
+				    " blocks) type: '", type, "'");
+			});
+
+			return true;
+		}
+
+		bool partition_valid(long num) const override
+		{
+			/* 1-based partition number to 0-based array index */
+			num -= 1;
+
+			if (num < 0 || num >= MAX_PARTITIONS)
+				return false;
+
+			return _part_list[num].constructed();
+		}
+
+		block_number_t partition_lba(long num) const override
+		{
+			return partition_valid(num) ? _part_list[num - 1]->lba : 0;
+		}
+
+		block_number_t partition_sectors(long num) const override
+		{
+			return partition_valid(num) ? _part_list[num - 1]->sectors : 0;
+		}
+
+		void generate_report(Xml_generator &xml) const override
+		{
+			auto gen_partition_attr = [&] (Xml_generator &xml, unsigned i)
+			{
+				Ahdi_partition const &part = *_part_list[i];
+
+				xml.attribute("number",     i + 1);
+				xml.attribute("start",      part.lba);
+				xml.attribute("length",     part.sectors);
+				xml.attribute("block_size", _info.block_size);
+				xml.attribute("type",       part.type);
+
+				if (part.fs_type.valid())
+					xml.attribute("file_system", part.fs_type);
+			};
+
+			xml.attribute("type", "ahdi");
+
+			_for_each_valid_partition([&] (unsigned i) {
+				xml.node("partition", [&] {
+					gen_partition_attr(xml, i); }); });
+		}
 };
 
 #endif /* _PART_BLOCK__AHDI_H_ */