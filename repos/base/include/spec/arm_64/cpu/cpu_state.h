cproc/genode

hw: send exception signals and support single-stepping
Fixes #4975

repos/base/include/spec/arm_64/cpu/cpu_state.h
@@ -22,9 +22,15 @@ namespace Genode { struct Cpu_state; }
 
 struct Genode::Cpu_state
 {
-	addr_t r[31] { 0 }; /* general purpose register 0...30 */
-	addr_t sp    { 0 }; /* stack pointer                   */
-	addr_t ip    { 0 }; /* instruction pointer             */
+	enum Cpu_exception {
+		SOFTWARE_STEP = 0x32,
+		BREAKPOINT    = 0x3c,
+	};
+
+	uint64_t r[31]  { 0 }; /* general purpose register 0...30 */
+	uint64_t sp     { 0 }; /* stack pointer                   */
+	uint64_t ip     { 0 }; /* instruction pointer             */
+	uint64_t ec     { 0 }; /* exception class                 */
 };
 
 #endif /* _INCLUDE__SPEC__ARM_64__CPU__CPU_STATE_H_ */

llaa33219/sponge-os

repos/base/include/spec/arm_64/cpu/cpu_state.h
@@ -22,9 +22,15 @@ namespace Genode { struct Cpu_state; }
 
 struct Genode::Cpu_state
 {
+	enum Cpu_exception {
+		SOFTWARE_STEP = 0x32,
+		BREAKPOINT    = 0x3c,
+	};
+
 	addr_t r[31] { 0 }; /* general purpose register 0...30 */
 	addr_t sp    { 0 }; /* stack pointer                   */
 	addr_t ip    { 0 }; /* instruction pointer             */
+	addr_t ec    { 0 }; /* exception class                 */
 };
 
 #endif /* _INCLUDE__SPEC__ARM_64__CPU__CPU_STATE_H_ */