ssumpf/genode

libc: implement kqueue(2)
Fixes #5301

repos/libports/src/test/kqueue/target.mk
@@ -1,0 +1,5 @@
+TARGET   = test-kqueue
+SRC_C    = kqueue.c
+LIBS     = posix
+
+CC_CXX_WARN_STRICT =

llaa33219/sponge-os

repos/libports/src/test/libc_kqueue/target.mk
@@ -1,0 +1,5 @@
+TARGET   = test-libc_kqueue
+SRC_C    = kqueue.c
+LIBS     = posix
+
+CC_CXX_WARN_STRICT =