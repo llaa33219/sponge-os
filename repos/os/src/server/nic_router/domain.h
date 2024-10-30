llaa33219/sponge-os

commit 698cfc94512cbdeb258b4be3f45f55066eff10ce

nic_router: remove use of exception handling
Remove the use of C++ exception as much as possible from the router as C++
exception handling can be resource intensive and can make code hard to
understand.

This also removes the garbage collection that the router used to do when a
session ran out of quota. This is motivated by the fact that the garbage
collection was rather simple and removed connection states regardless of their
current state, thereby causing broken connections. The change is part of this
commit as the approach to integrating garbage collection relied strongly on
exception handling.

The user story behind removing garbage collection: The router emergency-dropped
an established TCP connection (with NAPT) and on the next matching packet
re-created it with a different NAPT port, thereby breaking the connection. With
this commit, existing connections are prioritized over new ones during resource
exhaustion and the packets that attempt to create a new connection in such a
state are dropped with a warning in the log (verbose_packet_drop="yes").

Note that the state resolves itself with time as existing connections time out
or are closed by peers.

Ref #4729

repos/os/src/server/nic_router/domain.h
@@ -21,7 +21,6 @@
 #include <ip_rule.h>
 #include <arp_cache.h>
 #include <port_allocator.h>
-#include <pointer.h>
 #include <ipv4_config.h>
 #include <dhcp_server.h>
 #include <interface.h>
@@ -54,7 +53,8 @@ struct Net::Domain_object_stats
 
 	void dissolve_interface(Interface_object_stats const &stats);
 
-	void report(Genode::Xml_generator &xml);
+	bool report_empty() const;
+	void report(Genode::Xml_generator &xml) const;
 };
 
 
@@ -66,7 +66,8 @@ struct Net::Domain_link_stats : Domain_object_stats
 
 	void dissolve_interface(Interface_link_stats const &stats);
 
-	void report(Genode::Xml_generator &xml);
+	bool report_empty() const;
+	void report(Genode::Xml_generator &xml) const;
 };
 
 
@@ -74,26 +75,12 @@ class Net::Domain_dict : public Dictionary<Domain, Domain_name>
 {
 	public:
 
-		template <typename NO_MATCH_EXCEPTION>
-		Domain &deprecated_find_by_name(Domain_name const &domain_name)
+		void find_by_domain_attr(Genode::Xml_node const &node, auto const &ok_fn, auto const &failed_fn)
 		{
-			Domain *dom_ptr { nullptr };
-			with_element(
-				domain_name,
-				[&] /* match_fn */ (Domain &dom) { dom_ptr = &dom; },
-				[&] /* no_match_fn */ () { throw NO_MATCH_EXCEPTION { }; }
-			);
-			return *dom_ptr;
+			with_element(node.attribute_value("domain", Domain_name()),
+				[&] (Domain &domain) { ok_fn(domain); }, [&] { failed_fn(); });
 		}
 
-		template <typename NO_MATCH_EXCEPTION>
-		Domain &deprecated_find_by_domain_attr(Genode::Xml_node const &node)
-		{
-			Domain_name const domain_name {
-				node.attribute_value("domain", Domain_name { }) };
-
-			return deprecated_find_by_name<NO_MATCH_EXCEPTION>(domain_name);
-		}
 };
 
 
@@ -117,7 +104,7 @@ class Net::Domain : public List<Domain>::Element,
 		Nat_rule_tree                         _nat_rules            { };
 		Interface_list                        _interfaces           { };
 		unsigned long                         _interface_cnt        { 0 };
-		Pointer<Dhcp_server>                  _dhcp_server          { };
+		Dhcp_server                          *_dhcp_server_ptr      { };
 		Genode::Reconstructible<Ipv4_config>  _ip_config;
 		bool                            const _ip_config_dynamic    { !ip_config().valid() };
 		List<Domain>                          _ip_config_dependents { };
@@ -141,19 +128,19 @@ class Net::Domain : public List<Domain>::Element,
 		Domain_object_stats                   _dhcp_stats           { };
 		unsigned long                         _dropped_fragm_ipv4   { 0 };
 
-		void _read_forward_rules(Genode::Cstring  const &protocol,
-		                         Domain_dict            &domains,
-		                         Genode::Xml_node const  node,
-		                         char             const *type,
-		                         Forward_rule_tree      &rules);
+		[[nodiscard]] bool _read_forward_rules(Genode::Cstring  const &protocol,
+		                                       Domain_dict            &domains,
+		                                       Genode::Xml_node const  node,
+		                                       char             const *type,
+		                                       Forward_rule_tree      &rules);
 
-		void _read_transport_rules(Genode::Cstring  const &protocol,
-		                           Domain_dict            &domains,
-		                           Genode::Xml_node const  node,
-		                           char             const *type,
-		                           Transport_rule_list    &rules);
+		[[nodiscard]] bool _read_transport_rules(Genode::Cstring  const &protocol,
+		                                         Domain_dict            &domains,
+		                                         Genode::Xml_node const  node,
+		                                         char             const *type,
+		                                         Transport_rule_list    &rules);
 
-		void _invalid(char const *reason) const;
+		[[nodiscard]] bool _invalid(char const *reason) const;
 
 		void _log_ip_config() const;
 
@@ -171,11 +158,15 @@ class Net::Domain : public List<Domain>::Element,
 
 		void __FIXME__dissolve_foreign_arp_waiters();
 
+		/*
+		 * Noncopyable
+		 */
+		Domain(Domain const &);
+		Domain &operator = (Domain const &);
+
 	public:
 
-		struct Invalid          : Genode::Exception { };
-		struct Ip_config_static : Genode::Exception { };
-		struct No_next_hop      : Genode::Exception { };
+		struct Invalid : Genode::Exception { };
 
 		Domain(Configuration          &config,
 		       Genode::Xml_node const &node,
@@ -185,11 +176,21 @@ class Net::Domain : public List<Domain>::Element,
 
 		~Domain();
 
-		void init(Domain_dict &domains);
+		[[nodiscard]] bool finish_construction() const;
+
+		[[nodiscard]] bool init(Domain_dict &domains);
 
 		void deinit();
 
-		Ipv4_address const &next_hop(Ipv4_address const &ip) const;
+		void with_next_hop(Ipv4_address const &ip, auto const &ok_fn, auto const &error_fn) const
+		{
+			if (ip_config().interface().prefix_matches(ip))
+				ok_fn(ip);
+			else if (ip_config().gateway_valid())
+				ok_fn(ip_config().gateway());
+			else
+				error_fn();
+		}
 
 		void ip_config_from_dhcp_ack(Dhcp_packet &dhcp_ack);
 
@@ -209,7 +210,9 @@ class Net::Domain : public List<Domain>::Element,
 
 		void raise_tx_bytes(Genode::size_t bytes) { _tx_bytes += bytes; }
 
-		void report(Genode::Xml_generator &xml);
+		bool report_empty(Report const &) const;
+
+		void report(Genode::Xml_generator &xml, Report const &) const;
 
 		void add_dropped_fragm_ipv4(unsigned long dropped_fragm_ipv4);
 
@@ -217,6 +220,20 @@ class Net::Domain : public List<Domain>::Element,
 
 		void update_ready_state();
 
+		void with_dhcp_server(auto const &dhcp_server_fn, auto const &no_dhcp_server_fn)
+		{
+			if (!_dhcp_server_ptr) {
+				no_dhcp_server_fn();
+				return;
+			}
+			if (_dhcp_server_ptr->has_invalid_remote_dns_cfg()) {
+				no_dhcp_server_fn();
+				return;
+			}
+			dhcp_server_fn(*_dhcp_server_ptr);
+		}
+
+		void with_dhcp_server(auto const &fn) { with_dhcp_server(fn, []{}); }
 
 		/*********
 		 ** log **
@@ -247,7 +264,6 @@ class Net::Domain : public List<Domain>::Element,
 		Nat_rule_tree               &nat_rules()                 { return _nat_rules; }
 		Interface_list              &interfaces()                { return _interfaces; }
 		Configuration               &config()              const { return _config; }
-		Dhcp_server                 &dhcp_server();
 		Arp_cache                   &arp_cache()                 { return _arp_cache; }
 		Arp_waiter_list             &foreign_arp_waiters()       { return _foreign_arp_waiters; }
 		Link_side_tree              &tcp_links()                 { return _tcp_links; }

repos/os/include/net/dhcp.h
@@ -205,26 +205,19 @@ class Net::Dhcp_packet
 		/**
 		 * Domain name server option
 		 */
-		class Dns_server : public Option
+		struct Dns_server : public Option
 		{
-			private:
+			static constexpr Code CODE = Code::DNS_SERVER;
 
-				Ipv4_address _dns_servers[0];
+			Dns_server(Genode::size_t len) : Option(CODE, (Genode::uint8_t)len) { }
 
-			public:
+			void for_each_address(auto const &fn) const
+			{
+				Ipv4_address const *dns_servers = (Ipv4_address const *)((Genode::addr_t)this + sizeof(*this));
+				for (unsigned idx = 0; idx < len() / sizeof(*dns_servers); idx++)
+					fn(dns_servers[idx]);
+			}
 
-				static constexpr Code CODE = Code::DNS_SERVER;
-
-				Dns_server(Genode::size_t len) : Option(CODE, (Genode::uint8_t)len) { }
-
-				void for_each_address(auto const &fn) const
-				{
-					for (unsigned idx = 0;
-					     idx < len() / sizeof(_dns_servers[0]); idx++) {
-
-						fn(_dns_servers[idx]);
-					}
-				}
 		};
 
 		/**
@@ -515,6 +508,24 @@ class Net::Dhcp_packet
 				ptr = (void *)((Genode::addr_t)ptr + sizeof(opt) + opt.len());
 			}
 		}
+
+		template <typename T>
+		void with_option(auto const &found_fn, auto const &not_found_fn) const
+		{
+			bool found = false;
+			for_each_option([&] (Option const &opt) {
+				if (found || opt.code() != T::CODE)
+					return;
+
+				found = true;
+				found_fn(*(T const *)(&opt));
+			});
+			if (!found)
+				not_found_fn();
+		}
+
+		template <typename T>
+		void with_option(auto const &fn) const { with_option<T>(fn, []{}); }
 
 
 		/***************

repos/os/src/server/nic_router/arp_cache.cc
@@ -69,7 +69,7 @@ void Arp_cache::new_entry(Ipv4_address const &ip, Mac_address const &mac)
 void Arp_cache::destroy_entries_with_mac(Mac_address const &mac)
 {
 	for (unsigned curr = 0; curr < NR_OF_ENTRIES; curr++) {
-		try {
+		if (_entries[curr].constructed()) {
 			Arp_cache_entry &entry = *_entries[curr];
 			if (entry.mac() != mac) {
 				continue;
@@ -79,8 +79,7 @@ void Arp_cache::destroy_entries_with_mac(Mac_address const &mac)
 			}
 			remove(&entry);
 			_entries[curr].destruct();
-
-		} catch (Arp_cache_entry_slot::Deref_unconstructed_object) { }
+		}
 	}
 }
 

repos/os/src/server/nic_router/assertion.h
@@ -1,0 +1,35 @@
+/*
+ * \brief  Assertion macros
+ * \author Martin Stein
+ * \date   2023-06-09
+ */
+
+/*
+ * Copyright (C) 2023 Genode Labs GmbH
+ *
+ * This file is part of the Genode OS framework, which is distributed
+ * under the terms of the GNU Affero General Public License version 3.
+ */
+
+#ifndef _ASSERTION_H_
+#define _ASSERTION_H_
+
+/* base includes */
+#include <base/log.h>
+#include <base/sleep.h>
+
+#define ASSERT(condition) \
+	do { \
+		if (!(condition)) { \
+			Genode::error(__FILE__, ":", __LINE__, ": ", " assertion \"", #condition, "\" failed "); \
+			Genode::sleep_forever(); \
+		} \
+	} while (false)
+
+#define ASSERT_NEVER_REACHED \
+	do { \
+		Genode::error(__FILE__, ":", __LINE__, ": ", " should have never been reached"); \
+		Genode::sleep_forever(); \
+	} while (false)
+
+#endif /* _ASSERTION_H_ */

repos/os/src/server/nic_router/bit_allocator_dynamic.h
@@ -17,6 +17,9 @@
 /* Genode includes */
 #include <base/allocator.h>
 
+/* local includes */
+#include <assertion.h>
+
 namespace Genode {
 
 	class Bit_array_dynamic;
@@ -26,14 +29,6 @@ namespace Genode {
 
 class Genode::Bit_array_dynamic
 {
-	public:
-
-		struct Invalid_bit_count              : Exception { };
-		struct Invalid_index_access           : Exception { };
-		struct Invalid_clear                  : Exception { };
-		struct Invalid_set                    : Exception { };
-		struct Count_of_bits_not_word_aligned : Exception { };
-
 	protected:
 
 		enum {
@@ -50,13 +45,14 @@ class Genode::Bit_array_dynamic
 		addr_t _word(addr_t index) const {
 			return index / BITS_PER_WORD; }
 
-		void _check_range(addr_t const index,
-		                  addr_t const width) const
+		[[nodiscard]] bool _check_range(addr_t const index,
+		                                addr_t const width) const
 		{
 			if ((index >= _word_cnt * BITS_PER_WORD) ||
 			    width > _word_cnt * BITS_PER_WORD ||
 			    _word_cnt * BITS_PER_WORD - width < index)
-				throw Invalid_index_access();
+				return false;
+			return true;
 		}
 
 		addr_t _mask(addr_t const index, addr_t const width,
@@ -71,9 +67,10 @@ class Genode::Bit_array_dynamic
 			                                : ((1UL << width) - 1) << shift;
 		}
 
-		void _set(addr_t index, addr_t width, bool free)
+		[[nodiscard]] bool _set(addr_t index, addr_t width, bool free)
 		{
-			_check_range(index, width);
+			if (!_check_range(index, width))
+				return false;
 
 			addr_t rest, word, mask;
 			do {
@@ -82,17 +79,18 @@ class Genode::Bit_array_dynamic
 
 				if (free) {
 					if ((_words[word] & mask) != mask)
-						throw Invalid_clear();
+						return false;
 					_words[word] &= ~mask;
 				} else {
 					if (_words[word] & mask)
-						throw Invalid_set();
+						return false;
 					_words[word] |= mask;
 				}
 
 				index = (_word(index) + 1) * BITS_PER_WORD;
 				width = rest;
 			} while (rest);
+			return true;
 		}
 
 	public:
@@ -103,7 +101,8 @@ class Genode::Bit_array_dynamic
 		 */
 		bool get(addr_t index, addr_t width) const
 		{
-			_check_range(index, width);
+			if (!_check_range(index, width))
+				return false;
 
 			bool used = false;
 			addr_t rest, mask;
@@ -117,23 +116,18 @@ class Genode::Bit_array_dynamic
 			return used;
 		}
 
-		void set(addr_t const index, addr_t const width) {
-			_set(index, width, false); }
+		[[nodiscard]] bool set(addr_t const index, addr_t const width) {
+			return _set(index, width, false); }
 
-		void clear(addr_t const index, addr_t const width) {
-			_set(index, width, true); }
+		[[nodiscard]] bool clear(addr_t const index, addr_t const width) {
+			return _set(index, width, true); }
 
 		Bit_array_dynamic(addr_t *addr, unsigned bits)
 		: _bit_cnt(bits), _word_cnt(_bit_cnt / BITS_PER_WORD),
 		  _words(addr)
 		{
-			if (!bits || bits % BITS_PER_WORD)
-				throw Invalid_bit_count();
-
+			ASSERT(bits && bits % BITS_PER_WORD == 0);
 			memset(_words, 0, sizeof(addr_t)*_word_cnt);
-
-			if (bits % BITS_PER_WORD)
-				throw Count_of_bits_not_word_aligned();
 		}
 };
 
@@ -164,11 +158,11 @@ class Genode::Bit_allocator_dynamic
 		 *
 		 * \noapi
 		 */
-		void _reserve(addr_t bit_start, size_t const num)
+		[[nodiscard]] bool _reserve(addr_t bit_start, size_t const num)
 		{
-			if (!num) return;
+			if (!num) return true;
 
-			_array.set(bit_start, num);
+			return _array.set(bit_start, num);
 		}
 
 		size_t _ram_size() const
@@ -178,50 +172,53 @@ class Genode::Bit_allocator_dynamic
 
 	public:
 
-		struct Out_of_indices : Exception { };
-		struct Range_conflict : Exception { };
+		struct Alloc_error { };
+		using Alloc_result = Attempt<Genode::addr_t, Alloc_error>;
 
-		addr_t alloc(size_t const num_log2 = 0)
+		[[nodiscard]] Alloc_result alloc()
 		{
-			addr_t const step = 1UL << num_log2;
+			addr_t const step = 1UL;
 			addr_t max = ~0UL;
 
 			do {
-				try {
-					/* throws exception if array is accessed outside bounds */
-					for (addr_t i = _next & ~(step - 1); i < max; i += step) {
-						if (_array.get(i, step))
-							continue;
+				for (addr_t i = _next & ~(step - 1); i < max; i += step) {
+					if (_array.get(i, step))
+						continue;
 
-						_array.set(i, step);
-						_next = i + step;
-						return i;
-					}
-				} catch (Bit_array_dynamic::Invalid_index_access) { }
+					if (!_array.set(i, step))
+						break;
 
+					_next = i + step;
+					return i;
+				}
 				max = _next;
 				_next = 0;
 
 			} while (max != 0);
 
-			throw Out_of_indices();
+			return Alloc_error();
 		}
 
-		void alloc_addr(addr_t const bit_start, size_t const num_log2 = 0)
+		[[nodiscard]] bool alloc_addr(addr_t const bit_start)
 		{
-			addr_t const step = 1UL << num_log2;
+			addr_t const step = 1UL;
 			if (_array.get(bit_start, step))
-				throw Range_conflict();
+				return false;
 
-			_array.set(bit_start, step);
+			if (!_array.set(bit_start, step))
+				return false;
+
 			_next = bit_start + step;
-			return;
+			return true;
 		}
 
-		void free(addr_t const bit_start, size_t const num_log2 = 0)
+		[[nodiscard]] bool free(addr_t const bit_start)
 		{
-			_array.clear(bit_start, 1UL << num_log2);
+			if (!_array.clear(bit_start, 1UL))
+				return false;
+
 			_next = bit_start;
+			return true;
 		}
 
 		Bit_allocator_dynamic(Allocator &alloc, unsigned bits)
@@ -233,7 +230,7 @@ class Genode::Bit_allocator_dynamic
 			_ram((addr_t *)_alloc.alloc(_ram_size())),
 			_array(_ram, _bits_aligned)
 		{
-			_reserve(bits, _bits_aligned - bits);
+			ASSERT(_reserve(bits, _bits_aligned - bits));
 		}
 
 		~Bit_allocator_dynamic()

repos/os/src/server/nic_router/configuration.cc
@@ -63,31 +63,20 @@ void Configuration::_invalid_domain(Domain     &domain,
 Icmp_packet::Code
 Configuration::_init_icmp_type_3_code_on_fragm_ipv4(Xml_node const &node) const
 {
-	char const *const attr_name { "icmp_type_3_code_on_fragm_ipv4" };
-	try {
-		Xml_attribute const &attr { node.attribute(attr_name) };
-		if (attr.has_value("no")) {
-			return Icmp_packet::Code::INVALID;
-		}
-		uint8_t attr_val { };
-		bool const attr_transl_succeeded { attr.value<uint8_t>(attr_val) };
-		Icmp_packet::Code const result {
-			Icmp_packet::code_from_uint8(
-				Icmp_packet::Type::DST_UNREACHABLE, attr_val) };
-
-		if (!attr_transl_succeeded ||
-		    result == Icmp_packet::Code::INVALID) {
-
-			warning("attribute 'icmp_type_3_code_on_fragm_ipv4' has invalid "
-			        "value, assuming value \"no\"");
-
-			return Icmp_packet::Code::INVALID;
-		}
+	using Attribute_string = String<16>;
+	Icmp_packet::Code result = Icmp_packet::Code::INVALID;
+	Attribute_string attr_str = node.attribute_value("icmp_type_3_code_on_fragm_ipv4", Attribute_string());
+	if (attr_str == "no" || attr_str == Attribute_string())
 		return result;
+
+	uint8_t attr_u8 { };
+	if (Genode::ascii_to(attr_str.string(), attr_u8) == attr_str.length() - 1) {
+		result = Icmp_packet::code_from_uint8(Icmp_packet::Type::DST_UNREACHABLE, attr_u8);
+		if (result != Icmp_packet::Code::INVALID)
+			return result;
 	}
-	catch (Xml_node::Nonexistent_attribute) {
-		return Icmp_packet::Code::INVALID;
-	}
+	warning("attribute 'icmp_type_3_code_on_fragm_ipv4' has invalid value");
+	return result;
 }
 
 
@@ -120,119 +109,101 @@ Configuration::Configuration(Env                             &env,
 {
 	/* do parts of domain initialization that do not lookup other domains */
 	node.for_each_sub_node("domain", [&] (Xml_node const node) {
-		try {
-			Domain_name const name {
-				node.attribute_value("name", Domain_name { }) };
+		Domain_name const name {
+			node.attribute_value("name", Domain_name { }) };
 
-			_domains.with_element(
-				name,
-				[&] /* match_fn */ (Domain &other_domain)
-				{
-					if (_verbose) {
+		_domains.with_element(
+			name,
+			[&] /* match_fn */ (Domain &other_domain)
+			{
+				if (_verbose) {
 
-						log("[", name,
-						    "] invalid domain (name not unique) ");
+					log("[", name,
+					    "] invalid domain (name not unique) ");
 
-						log("[", other_domain,
-						    "] invalid domain (name not unique) ");
-					}
-					destroy(_alloc, &other_domain);
-				},
-				[&] /* no_match_fn */ ()
-				{
-					new (_alloc) Domain {
-						*this, node, name, _alloc, _domains };
+					log("[", other_domain,
+					    "] invalid domain (name not unique) ");
 				}
-			);
-		}
-		catch (Domain::Invalid) { }
+				destroy(_alloc, &other_domain);
+			},
+			[&] /* no_match_fn */ ()
+			{
+				Domain &domain = *new (_alloc) Domain { *this, node, name, _alloc, _domains };
+				if (!domain.finish_construction())
+					destroy(_alloc, &domain);
+			}
+		);
 	});
 	/* do parts of domain initialization that may lookup other domains */
 	while (true) {
+		Domain *invalid_domain_ptr { };
+		_domains.for_each([&] (Domain &domain) {
+			if (invalid_domain_ptr)
+				return;
 
-		struct Retry_without_domain : Genode::Exception
-		{
-			Domain &domain;
+			if (!domain.init(_domains)) {
+				invalid_domain_ptr = &domain;
+				return;
+			}
+			if (_verbose) {
+				log("[", domain, "] initiated domain"); }
+		});
+		if (!invalid_domain_ptr)
+			break;
 
-			Retry_without_domain(Domain &domain) : domain(domain) { }
-		};
-		try {
-			_domains.for_each([&] (Domain &domain) {
-				try { domain.init(_domains); }
-				catch (Domain::Invalid) { throw Retry_without_domain(domain); }
-				if (_verbose) {
-					log("[", domain, "] initiated domain"); }
-			});
-		}
-		catch (Retry_without_domain exception) {
+		/* destroy domain that became invalid during initialization */
+		destroy(_alloc, invalid_domain_ptr);
 
-			/* destroy domain that became invalid during initialization */
-			destroy(_alloc, &exception.domain);
-
-			/* deinitialize the remaining domains again */
-			_domains.for_each([&] (Domain &domain) {
-				domain.deinit();
-				if (_verbose) {
-					log("[", domain, "] deinitiated domain"); }
-			});
-			/* retry to initialize the remaining domains */
-			continue;
-		}
-		break;
+		/* deinitialize the remaining domains again */
+		_domains.for_each([&] (Domain &domain) {
+			domain.deinit();
+			if (_verbose) {
+				log("[", domain, "] deinitiated domain"); }
+		});
 	}
-	try {
-		/* check whether we shall create a report generator */
-		Xml_node const report_node = node.sub_node("report");
-		try {
-			/* try to re-use existing reporter */
-			_reporter = old_config._reporter();
-			old_config._reporter = Pointer<Reporter>();
-		}
-		catch (Pointer<Reporter>::Invalid) {
-
+	node.with_optional_sub_node("report", [&] (Xml_node const &report_node) {
+		if (old_config._reporter_ptr) {
+			/* re-use existing reporter */
+			_reporter_ptr = old_config._reporter_ptr;
+			old_config._reporter_ptr = nullptr;
+		} else {
 			/* there is no reporter by now, create a new one */
-			_reporter = *new (_alloc) Reporter(env, "state", nullptr, 4096 * 4);
+			_reporter_ptr = new (_alloc) Reporter(env, "state", nullptr, 4096 * 4);
 		}
 		/* create report generator */
-		_report = *new (_alloc)
-			Report {
-				_verbose, report_node, timer, _domains, shared_quota, env.pd(),
-				_reporter(), report_signal_cap };
-	}
-	catch (Genode::Xml_node::Nonexistent_sub_node) { }
-
+		_report.construct(
+			_verbose, report_node, timer, _domains, shared_quota, env.pd(),
+			*_reporter_ptr, report_signal_cap);
+	});
 	/* initialize NIC clients */
 	_node.for_each_sub_node("nic-client", [&] (Xml_node const node) {
-		try {
-			Session_label const label {
-				node.attribute_value("label",  Session_label::String { }) };
+		Session_label const label {
+			node.attribute_value("label",  Session_label::String { }) };
 
-			Domain_name const domain {
-				node.attribute_value("domain", Domain_name { }) };
+		Domain_name const domain {
+			node.attribute_value("domain", Domain_name { }) };
 
-			_nic_clients.with_element(
-				label,
-				[&] /* match */ (Nic_client &nic_client)
-				{
-					if (_verbose) {
+		_nic_clients.with_element(
+			label,
+			[&] /* match */ (Nic_client &nic_client)
+			{
+				if (_verbose) {
 
-						log("[", domain, "] invalid NIC client: ",
-						    label, " (label not unique)");
+					log("[", domain, "] invalid NIC client: ",
+					    label, " (label not unique)");
 
-						log("[", nic_client.domain(), "] invalid NIC client: ",
-						    nic_client.label(), " (label not unique)");
-					}
-					destroy(_alloc, &nic_client);
-				},
-				[&] /* no_match */ ()
-				{
-					new (_alloc) Nic_client {
-						label, domain, alloc, old_config._nic_clients,
-						_nic_clients, env, timer, interfaces, *this };
+					log("[", nic_client.domain(), "] invalid NIC client: ",
+					    nic_client.label(), " (label not unique)");
 				}
-			);
-		}
-		catch (Nic_client::Invalid) { }
+				destroy(_alloc, &nic_client);
+			},
+			[&] /* no_match */ ()
+			{
+				Nic_client &nic_client = *new (_alloc) Nic_client { label, domain, alloc, _nic_clients, *this };
+				if (!nic_client.finish_construction(env, timer, interfaces, old_config._nic_clients))
+					destroy(_alloc, &nic_client);
+			}
+		);
 	});
 	/*
 	 * Destroy old NIC clients to ensure that NIC client interfaces that were
@@ -248,12 +219,8 @@ Configuration::~Configuration()
 	_nic_clients.destroy_each(_alloc);
 
 	/* destroy reporter */
-	try { destroy(_alloc, &_reporter()); }
-	catch (Pointer<Reporter>::Invalid) { }
-
-	/* destroy report generator */
-	try { destroy(_alloc, &_report()); }
-	catch (Pointer<Report>::Invalid) { }
+	if (_reporter_ptr)
+		destroy(_alloc, _reporter_ptr);
 
 	/* destroy domains */
 	_domains.destroy_each(_alloc);

commit f8a78e062d4ddbd5e9948167b2dbc1c9baaddf37

nic_router: fix DHCP deallocation on domain update
Commit 698cfc9 introduced a regression that triggered an assertion in
`Dhcp_server::free_ip()` because the DHCP allocation was not properly
removed during a domain update. The underlying issue was that
`with_dhcp_server()` silently landed in the `no_dhcp_server_fn`.

Fixes #5364

repos/os/src/server/nic_router/domain.h
@@ -232,7 +232,7 @@ class Net::Domain : public List<Domain>::Element,
 			dhcp_server_fn(*_dhcp_server_ptr);
 		}
 
-		void with_dhcp_server(auto const &fn) { with_dhcp_server(fn, []{}); }
+		void with_optional_dhcp_server(auto const &fn) { with_dhcp_server(fn, []{}); }
 
 		/*********
 		 ** log **

repos/os/src/server/nic_router/interface.cc
@@ -1734,7 +1734,7 @@ void Interface::_continue_handle_eth(Packet_descriptor const &pkt)
 void Interface::_destroy_dhcp_allocation(Dhcp_allocation &allocation,
                                          Domain          &local_domain)
 {
-	local_domain.with_dhcp_server([&] (Dhcp_server &srv) {
+	local_domain.with_optional_dhcp_server([&] (Dhcp_server &srv) {
 		srv.free_ip(allocation.ip()); });
 	destroy(_alloc, &allocation);
 }
@@ -2035,6 +2035,18 @@ void Interface::_update_dhcp_allocations(Domain &old_domain,
                                          Domain &new_domain)
 {
 	bool dhcp_clients_outdated { false };
+
+	auto dismiss_all_fn = [&] () {
+		dhcp_clients_outdated = true;
+		while (Dhcp_allocation *allocation = _dhcp_allocations.first()) {
+			if (_config_ptr->verbose())
+				log("[", new_domain, "] dismiss DHCP allocation: ",
+					*allocation, " (other/no DHCP server)");
+			_dhcp_allocations.remove(*allocation);
+			_destroy_dhcp_allocation(*allocation, old_domain);
+		}
+	};
+
 	old_domain.with_dhcp_server([&] (Dhcp_server &old_dhcp_srv) {
 	new_domain.with_dhcp_server([&] (Dhcp_server &new_dhcp_srv) {
 		if (old_dhcp_srv.config_equal_to_that_of(new_dhcp_srv)) {
@@ -2054,18 +2066,16 @@ void Interface::_update_dhcp_allocations(Domain &old_domain,
 			});
 		} else {
 			/* dismiss all DHCP allocations */
-			dhcp_clients_outdated = true;
-			while (Dhcp_allocation *allocation = _dhcp_allocations.first()) {
-				if (_config_ptr->verbose())
-					log("[", new_domain, "] dismiss DHCP allocation: ",
-						*allocation, " (other/no DHCP server)");
-				_dhcp_allocations.remove(*allocation);
-				_destroy_dhcp_allocation(*allocation, old_domain);
-			}
+			dismiss_all_fn();
 		}
-		if (dhcp_clients_outdated)
-			_reset_and_refetch_domain_ready_state();
-	});});
+	},
+	/* no DHCP server on new domain */
+	[&] () { dismiss_all_fn(); }); },
+	/* no DHCP server on old domain */
+	[&] () { dismiss_all_fn(); });
+
+	if (dhcp_clients_outdated)
+		_reset_and_refetch_domain_ready_state();
 }
 
 