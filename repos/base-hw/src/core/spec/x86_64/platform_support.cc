atopia/genode

hw: calibrate TSC via ACPI timer
To get the Time Stamp Counter's frequency, hw relied on a complex and
incomplete algorithm.

Since this is a one-time initialization issue, move TSC calibration to
bootstrap and implement it using the ACPI timer.

Issue #5215

repos/base-hw/src/core/spec/x86_64/platform_support.cc
@@ -59,8 +59,8 @@ void Platform::_init_additional_platform_info(Xml_generator &xml)
 			xml.attribute("vmx", Hw::Virtualization_support::has_vmx());
 		});
 		xml.node("tsc", [&] {
-			xml.attribute("invariant", Hw::Lapic::invariant_tsc());
-			xml.attribute("freq_khz", Hw::Lapic::tsc_freq());
+			xml.attribute("invariant", Hw::Tsc::invariant_tsc());
+			xml.attribute("freq_khz", _boot_info().plat_info.tsc_frequency);
 		});
 	});
 }

repos/base-hw/src/include/hw/spec/x86_64/pc_board.h
@@ -40,10 +40,11 @@ struct Hw::Pc_board::Serial : Genode::X86_uart
 
 struct Hw::Pc_board::Boot_info
 {
-	Acpi_rsdp      acpi_rsdp        { };
-	Framebuffer    framebuffer      { };
-	Genode::addr_t efi_system_table { 0 };
-	Genode::addr_t acpi_fadt        { 0 };
+	Acpi_rsdp        acpi_rsdp        { };
+	Framebuffer      framebuffer      { };
+	Genode::addr_t   efi_system_table { 0 };
+	Genode::addr_t   acpi_fadt        { 0 };
+	Genode::uint32_t tsc_frequency    { 0 };
 
 	Boot_info() {}
 	Boot_info(Acpi_rsdp    const &acpi_rsdp,

llaa33219/sponge-os

repos/base-hw/src/core/spec/x86_64/platform_support.cc
@@ -59,8 +59,8 @@ void Platform::_init_additional_platform_info(Xml_generator &xml)
 			xml.attribute("vmx", Hw::Virtualization_support::has_vmx());
 		});
 		xml.node("tsc", [&] {
-			xml.attribute("invariant", Hw::Lapic::invariant_tsc());
-			xml.attribute("freq_khz", Hw::Lapic::tsc_freq());
+			xml.attribute("invariant", Hw::Tsc::invariant_tsc());
+			xml.attribute("freq_khz", _boot_info().plat_info.tsc_freq_khz);
 		});
 	});
 }

repos/base-hw/src/include/hw/spec/x86_64/pc_board.h
@@ -40,10 +40,11 @@ struct Hw::Pc_board::Serial : Genode::X86_uart
 
 struct Hw::Pc_board::Boot_info
 {
-	Acpi_rsdp      acpi_rsdp        { };
-	Framebuffer    framebuffer      { };
-	Genode::addr_t efi_system_table { 0 };
-	Genode::addr_t acpi_fadt        { 0 };
+	Acpi_rsdp        acpi_rsdp        { };
+	Framebuffer      framebuffer      { };
+	Genode::addr_t   efi_system_table { 0 };
+	Genode::addr_t   acpi_fadt        { 0 };
+	Genode::uint32_t tsc_freq_khz     { 0 };
 
 	Boot_info() {}
 	Boot_info(Acpi_rsdp    const &acpi_rsdp,