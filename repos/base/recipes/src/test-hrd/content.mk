nfeske/genode

Parser and generator for Human-Readable Data (HRD)
Issue #5635

repos/base/recipes/src/test-hrd/content.mk
@@ -1,0 +1,2 @@
+SRC_DIR = src/test/hrd_node
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc

repos/base/src/test/hrd/target.mk
@@ -1,0 +1,3 @@
+TARGET = test-hrd_node
+SRC_CC = main.cc
+LIBS  += base

llaa33219/sponge-os

repos/base/recipes/src/test-hrd/content.mk
@@ -1,0 +1,2 @@
+SRC_DIR = src/test/hrd
+include $(GENODE_DIR)/repos/base/recipes/src/content.inc

repos/base/src/test/hrd/target.mk
@@ -1,0 +1,3 @@
+TARGET = test-hrd
+SRC_CC = main.cc
+LIBS  += base