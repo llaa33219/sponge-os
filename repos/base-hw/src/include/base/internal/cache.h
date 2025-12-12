ssumpf/genode

base: replace get_page_size() by plain constant
The internal base/internal/page_size.h header provides the
'get_page_size()' and 'get_page_size_log()' functions, which used to be
actual function calls in the past (coming from base-pistachio where the
page size was obtained from the KIP) but changed to constexpr later.
This patch turns those to the plain constants PAGE_SIZE_LOG2, PAGE_SIZE,
and PAGE_MASK, thereby simplifying the code using them.

Fixes #5762

repos/base-hw/src/include/base/internal/cache.h
@@ -25,7 +25,7 @@ static inline void for_each_page(Genode::addr_t addr,
 	using namespace Genode;
 
 	while (size) {
-		addr_t next_page = align_addr(addr+1, get_page_size_log2());
+		addr_t next_page = align_addr(addr+1, PAGE_SIZE_LOG2);
 		size_t s = min(size, next_page - addr);
 		touch_read(reinterpret_cast<unsigned char *>(addr));
 		fn(addr, s);

llaa33219/sponge-os

repos/base-hw/src/include/base/internal/cache.h
@@ -25,7 +25,7 @@ static inline void for_each_page(Genode::addr_t addr,
 	using namespace Genode;
 
 	while (size) {
-		addr_t next_page = align_addr(addr+1, { .log2 = get_page_size_log2() });
+		addr_t next_page = align_addr(addr+1, AT_PAGE);
 		size_t s = min(size, next_page - addr);
 		touch_read(reinterpret_cast<unsigned char *>(addr));
 		fn(addr, s);