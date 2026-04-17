jschlatow/genode

nitpicker: move condition into utility
Issue #5716

repos/os/src/server/nitpicker/user_state.cc
@@ -27,6 +27,14 @@ static inline bool _mouse_button(Keycode keycode) {
 		return keycode >= BTN_LEFT && keycode <= BTN_MIDDLE; }
 
 
+static inline bool _hovered_takes_input(View_owner const *hovered,
+                                        View_owner const *focused)
+{
+	return hovered && (hovered->has_focusable_domain()
+	                || hovered->has_same_domain(focused));
+}
+
+
 /**
  * Determine number of events that can be merged into one
  *
@@ -184,11 +192,8 @@ void User_state::_handle_input_event(Input::Event ev)
 		_last_clicked = nullptr;
 
 		/* update focused session */
-		if (_mouse_button(keycode)
-		 && _hovered
-		 && (_hovered != _focused)
-		 && (_hovered->has_focusable_domain()
-		  || _hovered->has_same_domain(_focused))) {
+		if (_mouse_button(keycode) && (_hovered != _focused)
+		 && _hovered_takes_input(_hovered, _focused)) {
 
 			/*
 			 * Notify both the old focused session and the new one.
@@ -319,9 +324,7 @@ void User_state::_handle_input_event(Input::Event ev)
 			return;
 
 		if (!_mouse_button(key) || _global_key_sequence
-		 || (_hovered
-		  && (_hovered->has_focusable_domain()
-		   || _hovered->has_same_domain(_focused))))
+		 || _hovered_takes_input(_hovered, _focused))
 			_input_receiver->submit_input_event(ev);
 		else
 			_input_receiver = nullptr;

llaa33219/sponge-os

repos/os/src/server/nitpicker/user_state.cc
@@ -27,6 +27,14 @@ static inline bool _mouse_button(Keycode keycode) {
 		return keycode >= BTN_LEFT && keycode <= BTN_MIDDLE; }
 
 
+static inline bool _takes_input(View_owner const *owner,
+                                View_owner const *focused)
+{
+	return owner && (owner->has_focusable_domain()
+	              || owner->has_same_domain(focused));
+}
+
+
 /**
  * Determine number of events that can be merged into one
  *
@@ -184,11 +192,8 @@ void User_state::_handle_input_event(Input::Event ev)
 		_last_clicked = nullptr;
 
 		/* update focused session */
-		if (_mouse_button(keycode)
-		 && _hovered
-		 && (_hovered != _focused)
-		 && (_hovered->has_focusable_domain()
-		  || _hovered->has_same_domain(_focused))) {
+		if (_mouse_button(keycode) && (_hovered != _focused)
+		 && _takes_input(_hovered, _focused)) {
 
 			/*
 			 * Notify both the old focused session and the new one.
@@ -319,9 +324,7 @@ void User_state::_handle_input_event(Input::Event ev)
 			return;
 
 		if (!_mouse_button(key) || _global_key_sequence
-		 || (_hovered
-		  && (_hovered->has_focusable_domain()
-		   || _hovered->has_same_domain(_focused))))
+		 || _takes_input(_hovered, _focused))
 			_input_receiver->submit_input_event(ev);
 		else
 			_input_receiver = nullptr;